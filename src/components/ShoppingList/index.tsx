f3ddf3e7fe581c95b949519f615c297e79bfed7a
Julio Gaiotto <julio.gaiotto@datum.inf.br>
2022-04-20 10:43:18 -0300

    limitando o total de documentos retornados pela consulta

--- src/components/ShoppingList/index.tsx
+++ src/components/ShoppingList/index.tsx
@@ -11,7 +11,7 @@ export function ShoppingList() {
   useEffect(() => {
     const subscribe = firestore()
     .collection('products')
-    .where('quantity', '==', 3)
+    .limit(2)
     .onSnapshot(querySnapshot => {
       const data = querySnapshot.docs.map((doc) => {
         return {
